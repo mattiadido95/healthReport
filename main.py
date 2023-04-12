87deb72d74f35e5abb0ce7204ba2106b50e49d4a
mattia.didonato95 <mattia.didonato95@gmail.com>
2023-04-12 20:16:16 +0200

    create output

--- main.py
+++ main.py
@@ -1,52 +1,213 @@
-import xml.etree.ElementTree as ET
-import json
-import logging
+"""
+applehealthdata.py: Extract data from Apple Health App's export.xml.
+
+Copyright (c) 2016 Nicholas J. Radcliffe
+Licence: MIT
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import re
 import sys
 
-# Crea il logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from xml.etree import ElementTree
+from collections import Counter, OrderedDict
 
-# Crea il gestore di log per scrivere i messaggi sulla console
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setLevel(logging.DEBUG)
+__version__ = '1.0'
 
-# Crea il formattatore dei messaggi di log
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
+FIELDS = OrderedDict((
+    ('sourceName', 's'),
+    ('sourceVersion', 's'),
+    ('device', 's'),
+    ('type', 's'),
+    ('unit', 's'),
+    ('creationDate', 'd'),
+    ('startDate', 'd'),
+    ('endDate', 'd'),
+    ('value', 'n'),
+))
 
-# Aggiungi il gestore di log al logger
-logger.addHandler(console_handler)
-
-
-# Crea una funzione per la conversione del file XML in un dizionario Python
-def xml_to_dict(filename):
-    # Inizia a processare il file XML
-    logger.info(f'Inizio conversione del file {filename}')
-    context = ET.iterparse(filename, events=('start', 'end'))
-    _, root = next(context)
-    stack = [root]
-    for event, elem in context:
-        if event == 'start':
-            stack.append(elem)
-        elif event == 'end':
-            parent = stack[-2]
-            if elem.tag in parent:
-                if not isinstance(parent[elem.tag], list):
-                    parent[elem.tag] = [parent[elem.tag]]
-                parent[elem.tag].append(elem.attrib)
-            else:
-                parent[elem.tag] = elem.attrib
-            stack.pop()
-        # Scrivi un messaggio di log ogni 10000 elementi processati
-        if len(stack) == 1 and len(stack[0]) % 10000 == 0:
-            logger.debug(f'Processati {len(stack[0])} elementi')
-    return root.attrib
+PREFIX_RE = re.compile('^HK.*TypeIdentifier(.+)$')
+ABBREVIATE = True
+VERBOSE = True
 
 
-# Converte il file XML in un dizionario Python
-dict_data = xml_to_dict('data/export_cda.xml')
+def format_freqs(counter):
+    """
+    Format a counter object for display.
+    """
+    return '\n'.join('%s: %d' % (tag, counter[tag])
+                     for tag in sorted(counter.keys()))
 
-# Scrivi il file JSON
-with open('output/cda.json', 'w') as file:
-    json.dump(dict_data, file)
+
+def format_value(value, datatype):
+    """
+    Format a value for a CSV file, escaping double quotes and backslashes.
+
+    None maps to empty.
+
+    datatype should be
+        's' for string (escaped)
+        'n' for number
+        'd' for datetime
+    """
+    if value is None:
+        return ''
+    elif datatype == 's':  # string
+        return '"%s"' % value.replace('\\', '\\\\').replace('"', '\\"')
+    elif datatype in ('n', 'd'):  # number or date
+        return value
+    else:
+        raise KeyError('Unexpected format value: %s' % datatype)
+
+
+def abbreviate(s):
+    """
+    Abbreviate particularly verbose strings based on a regular expression
+    """
+    m = re.match(PREFIX_RE, s)
+    return m.group(1) if ABBREVIATE and m else s
+
+
+def encode(s):
+    """
+    Encode string for writing to file.
+    In Python 2, this encodes as UTF-8, whereas in Python 3,
+    it does nothing
+    """
+    return s.encode('UTF-8') if sys.version_info.major < 3 else s
+
+
+class HealthDataExtractor(object):
+    """
+    Extract health data from Apple Health App's XML export, export.xml.
+
+    Inputs:
+        path:      Relative or absolute path to export.xml
+        verbose:   Set to False for less verbose output
+
+    Outputs:
+        Writes a CSV file for each record type found, in the same
+        directory as the input export.xml. Reports each file written
+        unless verbose has been set to False.
+    """
+
+    def __init__(self, path, verbose=VERBOSE):
+        self.in_path = path
+        self.verbose = verbose
+        self.directory = os.path.abspath(os.path.split(path)[0])
+        with open('data/dataExport.xml') as f:
+            self.report('Reading data from %s . . . ' % path, end='\n')
+            self.data = ElementTree.parse(f)
+            self.report('done')
+        self.root = self.data._root
+        self.nodes = list(self.root)
+        self.n_nodes = len(self.nodes)
+        self.abbreviate_types()
+        self.collect_stats()
+
+    def report(self, msg, end='\n'):
+        if self.verbose:
+            print(msg, end=end)
+            sys.stdout.flush()
+
+    def count_tags_and_fields(self):
+        self.tags = Counter()
+        self.fields = Counter()
+        for record in self.nodes:
+            self.tags[record.tag] += 1
+            for k in record.keys():
+                self.fields[k] += 1
+
+    def count_record_types(self):
+        self.record_types = Counter()
+        for record in self.nodes:
+            if record.tag == 'Record':
+                self.record_types[record.attrib['type']] += 1
+
+    def collect_stats(self):
+        self.count_record_types()
+        self.count_tags_and_fields()
+
+    # def open_for_writing(self):
+    #     self.handles = {}
+    #     self.paths = []
+    #     for kind in self.record_types:
+    #         path = os.path.join(self.directory, '%s.csv' % abbreviate(kind))
+    #         f = open(path, 'w')
+    #         f.write(','.join(FIELDS) + '\n')
+    #         self.handles[kind] = f
+    #         self.report('Opening %s for writing' % path)
+
+    def open_for_writing(self):
+        self.handles = {}
+        self.paths = []
+        output_dir = os.path.join(os.getcwd(), 'output')
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        path = os.path.join(output_dir, 'export.csv')
+        for kind in self.record_types:
+            f = open(path, 'w')
+            f.write(','.join(FIELDS) + '\n')
+            self.handles[kind] = f
+            self.report('Opening %s for writing' % path)
+
+    def abbreviate_types(self):
+        """
+        Shorten types by removing common boilerplate text.
+        """
+        for node in self.nodes:
+            if node.tag == 'Record':
+                if 'type' in node.attrib:
+                    node.attrib['type'] = abbreviate(node.attrib['type'])
+
+    def write_records(self):
+        for node in self.nodes:
+            if node.tag == 'Record':
+                attributes = node.attrib
+                kind = attributes['type']
+                values = [format_value(attributes.get(field), datatype)
+                          for (field, datatype) in FIELDS.items()]
+                line = encode(','.join(values) + '\n')
+                self.handles[kind].write(line)
+
+    def close_files(self):
+        for (kind, f) in self.handles.items():
+            f.close()
+            self.report('Written %s data.' % abbreviate(kind))
+
+    def extract(self):
+        self.open_for_writing()
+        self.write_records()
+        self.close_files()
+
+    # def report_stats(self):
+    #     print('\nTags:\n%s\n' % format_freqs(self.tags))
+    #     print('Fields:\n%s\n' % format_freqs(self.fields))
+    #     print('Record types:\n%s\n' % format_freqs(self.record_types))
+
+
+    def report_stats(self):
+        # Apri il file report.txt in modalità "append" per scrivere alla fine del file
+        with open("output/report.txt", "a") as report_file:
+            report_file.write('\nTags:\n%s\n' % format_freqs(self.tags))
+            report_file.write('Fields:\n%s\n' % format_freqs(self.fields))
+            report_file.write('Record types:\n%s\n\n' % format_freqs(self.record_types))
+
+            # Stampa il report anche sulla console
+            print('\nTags:\n%s\n' % format_freqs(self.tags))
+            print('Fields:\n%s\n' % format_freqs(self.fields))
+            print('Record types:\n%s\n' % format_freqs(self.record_types))
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) != 2:
+    #     print('USAGE: python applehealthdata.py /path/to/export.xml',
+    #           file=sys.stderr)
+    #     sys.exit(1)
+    data = HealthDataExtractor('/data/dataExport.xml')
+    data.report_stats()
+    data.extract()
